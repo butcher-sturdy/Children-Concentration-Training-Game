b8f7acf3e2e5c3ae84688f1927810b64aacb1471
byxdm <2079238395@qq.com>
2026-05-05 00:53:13 +0800

    关卡全部完成，稍微更正了jet逻辑

--- G/element/gizmo/jet_vent.gd
+++ G/element/gizmo/jet_vent.gd
@@ -1,19 +1,24 @@
 extends Area2D
 
-@export var boost_strength: float = 1200.0 
+@export var boost_strength: float = 1200.0 # 向上的升力
+@export var forward_push: float = 200.0    # 向前的推力
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
-
-	print("【雷达】检测到物体：", body.name) 
 	
 	if body.is_in_group("Player") or body.name == "player":
+		print("【喷气口】成功捕捉到玩家！")
+		
+		
+		body.global_position.y -= 20
+		
 		if "velocity" in body:
-	
-			body.global_position.y -= 10 
 			
-	
-			body.velocity.y = -boost_strength
-			print("起飞咯！当前吹力：", boost_strength)
+			body.velocity = Vector2(forward_push, -boost_strength)
+			
+			
+			body.set_deferred("velocity", Vector2(forward_push, -boost_strength))
+			
+			print("起飞！升力：", boost_strength, " 推力：", forward_push)
